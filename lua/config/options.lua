817cf364b0eea1f8a8d079338c04d82454946465
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-02 10:48:38 +0300

    feat(plugins/): add codesnap and cargo.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,7 +18,7 @@ opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 opt.cursorline = true -- highlight the current cursor line
 opt.termguicolors = true -- Enable termguicolors.
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.background = "light" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
